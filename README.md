bbc6f4e09a18293bc1f48fd8bef1fb08c6c9d85b
A4-Tacks <wdsjxhno1001@163.com>
2025-08-02 14:35:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Similar to [`std::cell::LazyCell`], but use `&mut self` to get or initialization
+
+Suitable for being wrapped in a `Mutex`
+
+# Examples
+
+```rust
+use lazymut::LazyMut;
+use std::sync::Mutex;
+
+static FOO: Mutex<LazyMut<Vec<i32>>> = Mutex::new(LazyMut::new(|| {
+    vec![1]
+}));
+
+let mut lock = FOO.lock().unwrap();
+assert_eq!(lock.get(), &mut vec![1]);
+lock.get().push(2);
+assert_eq!(lock.get(), &mut vec![1, 2]);
+```
---
+++ src/lib.rs
@@ -0,0 +1,106 @@
+#![doc = include_str!("../README.md")]
+#![no_std]
+
+use core::{fmt::{self, Debug}, mem::replace};
+
+#[doc = include_str!("../README.md")]
+#[derive(Default)]
+pub struct LazyMut<T, F = fn() -> T> {
+    state: State<T, F>,
+}
+
+impl<T: Debug, F> Debug for LazyMut<T, F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut t = f.debug_tuple("LazyMut");
+        match self.state.try_get() {
+            Some(val) => t.field(val),
+            None => t.field(&format_args!("<uninit>")),
+        };
+        t.finish()
+    }
+}
+
+impl<T, F> LazyMut<T, F> {
+    /// Creates a new [`LazyMut`]
+    pub const fn new(f: F) -> Self {
+        Self { state: State::Uninit(f) }
+    }
+
+    /// Try get inner value reference
+    pub const fn try_get(&self) -> Option<&T> {
+        self.state.try_get()
+    }
+
+    /// Try get inner value mut reference
+    pub const fn try_get_mut(&mut self) -> Option<&mut T> {
+        self.state.try_get_mut()
+    }
+
+    /// Returns value when initialized
+    pub fn into_inner(self) -> Option<T> {
+        match self.state {
+            State::Uninit(_) | State::Poisoned => None,
+            State::Inited(val) => Some(val),
+        }
+    }
+}
+
+impl<T, F: FnOnce() -> T> LazyMut<T, F> {
+    pub fn get(&mut self) -> &mut T {
+        self.state.get_or_init()
+    }
+}
+
+#[derive(Clone)]
+enum State<T, F> {
+    Uninit(F),
+    Inited(T),
+    Poisoned,
+}
+
+impl<T: Default, F> Default for State<T, F> {
+    fn default() -> Self {
+        Self::Inited(Default::default())
+    }
+}
+
+impl<T, F> State<T, F>
+where F: FnOnce() -> T,
+{
+    fn get_or_init(&mut self) -> &mut T {
+        match self {
+            State::Poisoned => panic_poisoned(),
+            State::Inited(val) => val,
+            State::Uninit(_) => {
+                let this = replace(self, Self::Poisoned);
+                let Self::Uninit(f) = this else { unreachable!() };
+                *self = State::Inited(f());
+                self.try_get_mut().unwrap()
+            },
+        }
+    }
+}
+
+impl<T, F> State<T, F> {
+    const fn try_get(&self) -> Option<&T> {
+        if let Self::Inited(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    const fn try_get_mut(&mut self) -> Option<&mut T> {
+        if let Self::Inited(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
+#[cold]
+#[inline(never)]
+fn panic_poisoned() -> ! {
+    panic!("LazyMut instance has previously been poisoned")
+}
